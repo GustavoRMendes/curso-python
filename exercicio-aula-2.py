a05c0e08387970dc0c19ddae6073e0d70d2d5a25
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-09 16:47:30 -0400

    Add files via upload

---
+++ exercicio-aula-2.py
@@ -0,0 +1,19 @@
+'''
+ Faça um formulário que pergunte o nome, cpf, endereço, idade,
+ altura, telefone e imprima em um relatótio organizado.
+'''
+
+
+nome = input("Digite seu nome: ")
+cpf = input("Digite seu CPF: ")
+endereco = input("Digite seu endereço: ")
+idade = input("Digite sua idade: ")
+altura = input("Digite sua altura: ")
+telefone = input("Digite seu telefone: ")
+print("\tRelatório")
+print("Nome: ", nome)
+print("CPF: ", cpf)
+print("Endereço: ", endereco)
+print("Idade: ", idade)
+print("Altura: ", altura)
+print("Telefone: ", telefone)
