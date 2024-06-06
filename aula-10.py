8779e4faff3908695db709b17419348cb65cb121
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-06 18:44:58 -0400

    Add files via upload

---
+++ aula-10.py
@@ -0,0 +1,8 @@
+## Arquivo de texto
+arquivo = open("arquivo.txt",'w')
+## arquivo = open("arquivo.txt",'a')
+## arquivo = open("arquivo.txt",'b')
+## arquivo = open("arquivo.txt",'rb')
+
+type(print(arquivo))
+arquivo.write("Eai pessoal")
